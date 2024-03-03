6d4246bc4a297fe563e7c3b9a9918d6dfbd13219
rameezraja0 <rameezraja1730482@gmail.com>
2024-03-03 16:51:53 +0500

    2nd Commit Working on Temperature component.

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Temperature from './components/Temperature'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-[#1f213a] h-screen flex justify-center align-top'>
+    <div className="left mt-40 w-1/5 h-1/3">
+      <Temperature/>
+    </div>
+    <div className="right bg-blue-400 mt-40 w-1/3 h-1/3">right</div>
+    </div>
   )
 }
 
